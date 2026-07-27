4f0f9fc9e5ee7e08f0fb9103fe9040baeb76f80b
ros002x <rosariotrupolo@gmail.com>
2026-07-27 21:19:34 +0200

    Prevent stale mobile layout cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "saldo-demo-v25";
+const CACHE_NAME = "saldo-demo-v26";
 const CORE_ASSETS = [
   "./",
   "index.html",
@@ -31,5 +31,13 @@ self.addEventListener("fetch", (event) => {
     return;
   }
 
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
